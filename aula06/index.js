7a077c60986439258032ff423ccbebd8fbc32bbb
Ernane Moreira <moreiraernane@hotmail.com>
2024-05-31 21:44:44 -0300

    aula06-variaveis com let

---
+++ aula06/index.js
@@ -0,0 +1,11 @@
+let nome = 'João';
+console.log(`${nome} nasceu em 1998`);
+console.log(`Em 2000 ${nome} conheceu Maria.`);
+console.log(`${nome} casou-se com Maria em 2012.`);
+console.log(`Maria teve 1 filho com ${nome}.`);
+console.log(`O filho de ${nome} se chama Eduardo.`);
+
+let nome1; //declarou a variável
+console.log(nome1)
+nome1 = 'Qualquer vador';
+console.log(nome1)
